37a2f3c62c26b6eeff4a5673f0b88aa6668dcc36
M-erick <muriithierick3758@gmail.com>
2024-04-27 14:14:39 +0300

    default header components,update later

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <router-link to="/">
+    <h1>MoviesLite
+  </h1>
+    </router-link>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+header{
+  display: flex;
+  align-items: left;
+  justify-content: left;
+  padding:10px 16px;
+  background-color: #2C3D4E;
+  box-shadow: 0px 0px 6px rgba(0,0,0,0.1);
+
+h1 {
+  color:#FFF;
+  font-size: 28px;
+    color:crimson;
+    // use google fonts here
+    font-family:sans 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+}
+</style>
